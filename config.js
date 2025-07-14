6105f6ddc9887adc962242dfb3a8a44e525c0840
bahrom-903 <bahrom.rahmonov@internet.ru>
2025-07-14 15:27:17 +0300

    config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-
+// config.js: Здесь будут все наши константы и настройки
+export const DB_NAME = 'AnimeGalleryDB_V22_Polished';
+export const DB_VERSION = 1;
+// ...сюда мы потом перенесем остальные константы
