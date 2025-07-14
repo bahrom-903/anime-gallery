492bd0abb568e99bec2f5150710468274ae8e494
bahrom-903 <bahrom.rahmonov@internet.ru>
2025-07-14 15:41:05 +0300

    state.js

--- state.js
+++ state.js
@@ -1,1 +1,25 @@
+// ===================================
+//      Файл: state.js
+// ===================================
 
+// Это объект, где будет храниться вся "память" приложения
+const state = {
+    currentSort: 'date_desc',
+    isFavFilterActive: false,
+    currentCategory: 'waifu',
+    currentLanguage: 'ru',
+    contextedItemId: null,
+    lastAiResult: null,
+};
+
+// Мы экспортируем не сам объект, а функцию, чтобы никто случайно не изменил его напрямую
+export const getState = () => state;
+
+// И функции для безопасного изменения состояния
+export const setState = (key, value) => {
+    if (key in state) {
+        state[key] = value;
+    } else {
+        console.error(`Попытка установить несуществующее состояние: ${key}`);
+    }
+};
